e200a50cb40f969cb0403a2e5cb7435d3b1adfbf
Irene Scott <irenejinscott@gmail.com>
2020-05-11 22:11:31 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -3,8 +3,8 @@ def second_challenge
     :montague => { 
     :partriarch => {},
     :matriarch => {},
-    :hero 
-    :hero_friends
+    :hero => {},
+    :hero_friends => []
       }, 
     :capulet => {
       :partriarch
